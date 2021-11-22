5374095e9b7ccc3c6df9985202d0f102274a1a55
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2021-11-22 21:00:43 +0800

    replenishment-components

---
+++ 2104/components/replenishment/rep-information.php
@@ -0,0 +1,54 @@
+<div class="to-confirm white-box-container-details-card-status bg-secondary mt-1 ">Shipping</div>
+<div class="white-box-container-details-card-body rounded">
+    <div class="row d-flex justify-content-between">
+        <div class="col">
+            <h4><b>$ProductID</b></h3>
+        </div>
+        <div class="col-md-auto">
+            $OrderDate
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-6 col-md-4">
+            Supplier ID<br>
+            Created By<br>
+            Approved By<br>
+            Date Shipped
+        </div>
+        <div class="col-12 col-sm-6 col-md-8">
+            <b>
+                $Supplier ID<br>
+                $Created By<br>
+                $Approved By<br>
+                $Date Shipped
+            </b>
+        </div>
+    </div>
+    <hr>
+    <div class="row d-flex justify-content-between">
+        <div class="col">
+            $tradeName<br>
+            $ProductName
+        </div>
+
+        <div class="col-md-auto d-flex justify-content-end">
+            x $Quantity
+        </div>
+        <div class="col-md-auto d-flex justify-content-end">
+            PHP $Price
+        </div>
+        <div class="col col-lg-3 d-flex justify-content-end">
+            PHP $total
+        </div>
+    </div>
+    <hr>
+    <div class="row d-flex justify-content-between">
+        <div class="col">
+            $TotalPrice
+        </div>
+        <div class="col-4 d-flex justify-content-end">
+            <b>$PHP 25,600.00</b>
+        </div>
+    </div>
+</div>
---
+++ 2104/components/replenishment/rep-list.php
@@ -0,0 +1,127 @@
+
+<?php
+// include('db/dbh.php');
+// session_start();
+?>
+
+<div class="">
+    <?php
+        echo "
+        
+
+
+
+
+        <div class='container-header d-flex'>
+                <div class='list-cell'>
+                        Order ID
+                </div>
+                
+                <div class='list-cell'>
+                        Order By
+                </div>
+                
+                <div class='list-cell'>
+                        Approved By
+                </div>
+
+                <div class='list-cell'>
+                        Order Date
+                </div>
+
+                <div class='list-cell'>
+                        Contact Person
+                </div>
+
+                <div class='list-cell'>
+                        Total Price
+                </div>
+
+                <div class='list-cell'>
+                        
+                </div>
+
+                <div class='list-cell'>
+                        
+                </div>
+
+                
+            </div>
+
+            ";
+            
+    ?>
+   <br>
+    <div class="scroll-list-2">
+
+        <!-- QUERRY TO SELECT ALL CUSTOMERS/SUPPLIER -->
+        <!-- <?php
+    
+                $OrderID = "1001";
+                $OrderBy = "Nikolai Tumapon";
+                $ApprovedBy = "Horeb Barriga";
+                $OrderDate = "2015-12-09";
+                $ContactPerson = "Van AJ Vanguardia";
+                $TotalPrice = "1000.00";
+
+
+                ?> -->
+
+        <?php
+        $SupplierID = "1001";
+        $SupplierName = "Nikolai Tumapon";
+
+
+        
+
+        for ($i = 0; $i < 10; $i++) {
+            echo "
+
+                    <form method='get' action='customers.php'>
+                        <div class='d-flex align-items-center justify-content-between white-box-container one-order-replenishment-list round-edge'>
+                            <div class='list-cell'>
+                                <b>"."$OrderID"."</b>
+                            </div>
+                            
+                            <div class='list-cell'>
+                                    $OrderBy
+                            </div>
+                            
+                            <div class='list-cell'>
+                                    $ApprovedBy
+                            </div>
+
+                            <div class='list-cell'>
+                                    $OrderDate
+                            </div>
+
+                            <div class='list-cell'>
+                                    $ContactPerson
+                            </div>
+
+                            <div class='list-cell'>
+                                $TotalPrice
+                            </div>
+
+                            <div class=''>
+                                    <form method='get' action=''>
+                                        <input type='hidden' name='' value=''>
+                                        <button type='submit' class='btn btn-link btn-update'>Update</button>
+                                    </form>
+                            </div>
+
+                            <div class=''>
+                                    <form method='get' action=''>
+                                        <input type='hidden' name='' value=''>
+                                        <button type='submit' class='btn btn-link btn-delete'>Delete</button>
+                                    </form>
+                            </div>
+
+                        </div>
+                    </form>
+                ";
+        }
+
+        ?>
+    </div>
+</div>
