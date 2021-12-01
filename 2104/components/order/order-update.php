ebd6442b5611f23e1ed4bcf4aaf2f2fc3cf379ed
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2021-12-01 14:14:36 +0800

    new

---
+++ 2104/components/order/order-update.php
@@ -0,0 +1,115 @@
+<?php
+    $class = "";
+
+    if ($order['orderStatus'] == "Completed") {
+        $class = "invisible";
+    }
+?>
+
+<div class="modal fade" id="<?php echo $modal ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Update Information</h5>
+                <a type="button" class="btn btn-outline-primary btn-sm <?php echo $class ?>" data-bs-dismiss="modal" aria-label="Close">
+                    <?php  echo substr($order['orderStatus'], 3); ?>
+                </a>
+            </div>
+            <div class="modal-body">
+            
+                <form class="row g-3" method="post" action="../crud/order/update-order.php">
+                    <b>Product Information</b>
+                    <?php
+                        include("../crud/order/get-orders.php")
+                    ?>
+
+                    <div class="col-md-6">
+                        <label class="col-form-label">Order Date</label>
+                        <input type="date" class="form-control" name="orderDate" value="<?php echo $order['orderDate'] ?>" />
+                    </div>
+
+                    <div class="col-md-6">
+                        <label class="col-form-label">Ship Required Date</label>
+                        <input type="date" class="form-control" name="shippingDate" value="<?php echo $order['shipRequiredDate'] ?>" />
+                    </div>
+
+                    <hr>
+                    <b>Customer Information</b>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">First Name</label>
+                        <input type="text" class="form-control" name="fname" value="<?php echo $order['customerFName'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Last Name</label>
+                        <input type="text" class="form-control" name="lname" value="<?php echo $order['customerLName'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Email Address</label>
+                        <input type="email" class="form-control" name="email" value="<?php echo $order['email'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Date of Birth</label>
+                        <input type="date" class="form-control" name="dob" value="<?php echo $order['dateofBirth'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Gender (M/F)</label>
+                        <input type="text" class="form-control" name="gender" value="<?php echo $order['gender'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Contact Number</label>
+                        <input type="text" class="form-control" name="contactNo" value="<?php echo $order['contactNo'] ?>" />
+                    </div>
+
+                    <hr>
+                    <b>Customer Address</b>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Street</label>
+                        <input class="form-control" name="street" value="<?php echo $order['street'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Barangay</label>
+                        <input type="text" class="form-control" name="barangay" value="<?php echo $order['barangay'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">City</label>
+                        <input type="text" class="form-control" name="city" value="<?php echo $order['city'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Region</label>
+                        <input class="form-control" name="region" value="<?php echo $order['region'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Country</label>
+                        <input type="text" class="form-control" name="country" value="<?php echo $order['country'] ?>" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Zip</label>
+                        <input type="text" class="form-control" name="zip" value="<?php echo $order['zip'] ?>" />
+                    </div>
+                
+                </div>
+
+                <div class="modal-footer">
+                    <input type='hidden' name='orderID' value=" <?php echo $order['orderID'] ?>">
+                    <input type='hidden' name='customerID' value=" <?php echo $order['customerID'] ?>">
+                    <input type='hidden' name='addressID' value=" <?php echo $order['addressID'] ?>">
+
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ 2104/components/replenishment/rep-update.php
@@ -0,0 +1,74 @@
+<?php
+    $class = "";
+
+    if ($rep['orderStatus'] == "Completed") {
+        $class = "invisible";
+    }
+?>
+
+<div class="modal fade" id="<?php echo $modal ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Update Information</h5>
+                <a type="button" class="btn btn-outline-primary btn-sm <?php echo $class ?>" data-bs-dismiss="modal" aria-label="Close">
+                    <?php  echo substr($rep['orderStatus'], 3); ?>
+                </a>
+            </div>
+                <div class="modal-body">
+
+                <form class="row g-3" method="post" action="../crud/replenishment/update-repOrder.php">
+                    <b>Product Information</b>
+                    <?php
+                        include("../crud/replenishment/get-repOrder.php")
+                    ?>
+
+                    <div class="col-md-6">
+                        <label class="col-form-label">Order Date</label>
+                        <input type="date" class="form-control" name="orderDate" value="<?php echo $rep['repOrderDate'] ?>" />
+                    </div>
+
+                    <div class="col-md-6">
+                        <label class="col-form-label">Ship Required Date</label>
+                        <input type="date" class="form-control" name="shippingDate" value="<?php echo $rep['shipRequiredDate'] ?>" />
+                    </div>
+
+                    <hr>
+                    <b>Supplier Information</b>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Company Name</label>
+                        <input type="text" class="form-control" name="companyName" value="<?php echo $rep['companyName'] ?>" disabled/>
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Contact First Name</label>
+                        <input type="text" class="form-control" name="contactFName" value="<?php echo $rep['contactFName'] ?>" disabled/>
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Contact Last Name</label>
+                        <input type="text" class="form-control" name="contactLName" value="<?php echo $rep['contactLName'] ?>" disabled/>
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Email Address</label>
+                        <input type="email" class="form-control" name="email" value="<?php echo $rep['email'] ?>" disabled/>
+                    </div>
+
+                    <div class="col-md-4">
+                        <label class="col-form-label">Contact Number</label>
+                        <input type="text" class="form-control" name="contactNo" value="<?php echo $rep['contactNo'] ?>" disabled/>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <input type='hidden' name='repOrderID' value=" <?php echo $rep['repOrderID'] ?>">
+
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>   
+        </div>
+    </div>
+</div>
