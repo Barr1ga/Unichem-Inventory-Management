758f11197ec71b4bdd1be51a0f39bfe422276cdb
Ceedz1 <84215937+Ceedz1@users.noreply.github.com>
2021-12-01 11:33:56 +0800

    Revert "Added sales to side bar"
    This reverts commit e30d9b26b8259230ad19db02062dc2ee4fd950f8.

--- 2104/components/sales/chart.php
+++ 2104/components/sales/chart.php
@@ -1,5 +1,9 @@
 <?php
-include('../crud/chart_db_connect.php');
+include('../crud/db_connect.php');
+$result = mysqli_query($conn, "SELECT *
+FROM (SELECT COUNT(orderID) AS orderCount, orderDate FROM orders GROUP BY EXTRACT(month FROM orderDate) ORDER BY orderDate ASC)A
+CROSS JOIN (SELECT COUNT(repOrderID) AS repOrderCount, repOrderDate FROM replenishment GROUP BY EXTRACT(month FROM repOrderDate))B
+ORDER BY orderDate")
 ?>
 
 
