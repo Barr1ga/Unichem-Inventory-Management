8e3a1eab47fe4471f16f225a21d3e7eed705b80e
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2021-12-02 17:47:30 +0800

    login update

--- 2104/sections/notifications.php
+++ 2104/sections/notifications.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(empty($_SESSION['userType'])){
-    header('Location: login.php');
+    header('Location: ../index.php');
 }
 include('../style/import.php');
 include('../crud/db_connect.php');
@@ -90,7 +90,7 @@ include('../crud/db_connect.php');
         <div class="container">
             <div class="row align-items-end">
                 <div class="col d-flex justify-content-center">
-                    <a href="login.php" class="d-flex align-items-center link-dark text-decoration-none">
+                    <a href="../index.php" class="d-flex align-items-center link-dark text-decoration-none">
                         <strong>Logout</strong>
                     </a>
                 </div>
