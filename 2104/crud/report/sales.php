ec3fd14ced9d92594e9eaa54a9643513113ff500
Ceedz1 <84215937+Ceedz1@users.noreply.github.com>
2021-12-02 18:33:03 +0800

    Update sales.php

--- 2104/crud/report/sales.php
+++ 2104/crud/report/sales.php
@@ -2,8 +2,8 @@
     include('../crud/db_connect.php');
 
     $query = "SELECT *
-                FROM (SELECT COUNT(orderID) AS orderCount, orderDate FROM orders GROUP BY EXTRACT(year FROM orderDate) ORDER BY orderDate ASC)A
-                CROSS JOIN (SELECT COUNT(repOrderID) AS repOrderCount, repOrderDate FROM replenishment GROUP BY EXTRACT(year FROM repOrderDate))B
+                FROM (SELECT COUNT(orderID) AS orderCount, orderDate FROM orders GROUP BY EXTRACT(month FROM orderDate) ORDER BY orderDate ASC)A
+                CROSS JOIN (SELECT COUNT(repOrderID) AS repOrderCount, repOrderDate FROM replenishment GROUP BY EXTRACT(month FROM repOrderDate))B
                 ORDER BY orderDate";
 
     $result = mysqli_query($conn, $query);
