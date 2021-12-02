91a2b0212ca1cf086d3c1a6a45a56dcc5bce19d3
Ceedz1 <84215937+Ceedz1@users.noreply.github.com>
2021-12-02 21:05:20 +0800

    Revert "Merge branch 'main' of https://github.com/Barr1ga/CIS2104-Inventory-Management"
    This reverts commit 77b902caaec6c52a4678612007b804b0c7487864, reversing
    changes made to ec3fd14ced9d92594e9eaa54a9643513113ff500.

--- 2104/components/inventory/product-info-update.php
+++ 2104/components/inventory/product-info-update.php
@@ -7,7 +7,7 @@
                         </div>
                         <div class="col-md-6">
                             <label for="inputID" class="form-label">Product ID</label>
-                            <input type="text" class="form-control" id="inputID" name="customerLName" required value="<?php echo $inventory['productID']; ?>">
+                            <input type="text" class="form-control" id="inputID" name="productID" required value="<?php echo $inventory['productID']; ?>">
                         </div>
                         <div class="mb-3">
                             <label for="inputDescription" class="form-label">Description</label>
@@ -28,52 +28,52 @@
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Application Type</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['applicationType']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="applicationType" required value="<?php echo $inventory['applicationType']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Cure Time</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['cureTime']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="cureTime" required value="<?php echo $inventory['cureTime']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Color</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['color']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="color" required value="<?php echo $inventory['color']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Form</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['form']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="form" required value="<?php echo $inventory['form']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Package Type</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['packageType']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="packageType" required value="<?php echo $inventory['packageType']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Package Size</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['packageSize']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="packageSize" required value="<?php echo $inventory['packageSize']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Min Operating Temp</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['minOperatingTemp']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="minOperatingTemp" required value="<?php echo $inventory['minOperatingTemp']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Max Operating Temp</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['maxOperatingTemp']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="macOperatingTemp" required value="<?php echo $inventory['maxOperatingTemp']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Viscosity</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['viscosity']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="viscosity" required value="<?php echo $inventory['viscosity']; ?>">
                         </div>
                         
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Temp Range</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['operatingTempRange']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="tempRange" required value="<?php echo $inventory['operatingTempRange']; ?>">
                         </div>
                         <div class="col-md-4">
-                            <label for="inputEmail4" class="form-label">Stock</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['inStock']; ?>">
+                            <label for="inputEmail4" class="form-label">In Stock</label>
+                            <input type="text" class="form-control" id="inputEmail4" name="inStock" required value="<?php echo $inventory['inStock']; ?>">
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail4" class="form-label">Chemical Composition</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="price" required value="<?php echo $inventory['chemicalComposition']; ?>">
+                            <input type="text" class="form-control" id="inputEmail4" name="chemicalComposition" required value="<?php echo $inventory['chemicalComposition']; ?>">
                         </div>
                         <<div class="mb-3">
                             <label for="formFile" class="form-label">Product Image</label>
