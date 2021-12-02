aa662c223c13f3a742f22724639008d69d633deb
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2021-12-02 13:17:47 +0800

    rep

--- 2104/crud/replenishment/update-repOrder.php
+++ 2104/crud/replenishment/update-repOrder.php
@@ -5,30 +5,32 @@
     $active = "../../sections/replenishments.php?repActive=".$_POST['defaultOrderStatus'];
 
     /* Replenishment Information */
-    if (!isset($_POST['orderStatus']))
-        $orderStatus = $_POST['defaultOrderStatus'];
-    else 
-        $orderStatus = $_POST['orderStatus'];
-
     $orderDate = $_POST['orderDate'];
     $shippingDate = $_POST['shippingDate'];
     $repOrderID = $_POST['repOrderID'];
     $product = $_POST['product'];
     $quantity = $_POST['quantity'];
 
-    if ($orderStatus == "Completed") {
-        foreach($product as $index => $name) {
-            $tradeName = $name;
-            $qty = $quantity[$index];
-
-            /* Query to update InStock */
-            $query = "UPDATE product
-                        SET inStock = inStock + '$qty'
-                        WHERE tradeName = '$tradeName' ";
-            if (mysqli_query($conn, $query)) {
-                echo '<br /> Product In Stock is successfully updated';
-            }else {
-                echo '<br /> Product In Stock is not successfully updated ' . mysqli_error($conn);
+    /* Update Order quantity if status is changed to completed*/
+    if (!isset($_POST['orderStatus'])) {
+        $orderStatus = $_POST['defaultOrderStatus'];
+    }
+    else {
+        $orderStatus = $_POST['orderStatus'];
+        if ($orderStatus == "Completed") {
+            foreach($product as $index => $name) {
+                $tradeName = $name;
+                $qty = $quantity[$index];
+    
+                /* Query to update InStock */
+                $query = "UPDATE product
+                            SET inStock = inStock + '$qty'
+                            WHERE tradeName = '$tradeName' ";
+                if (mysqli_query($conn, $query)) {
+                    echo '<br /> Product In Stock is successfully updated';
+                }else {
+                    echo '<br /> Product In Stock is not successfully updated ' . mysqli_error($conn);
+                }
             }
         }
     }
