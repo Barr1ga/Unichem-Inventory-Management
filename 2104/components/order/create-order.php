f1a92e0da0efbb8782fe3ace96f2a64eca3a9ecd
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2021-12-03 11:15:18 +0800

    create-order validation

--- 2104/components/order/create-order.php
+++ 2104/components/order/create-order.php
@@ -18,7 +18,7 @@
                                 echo "<option value=" . $row['productID'] . ">" ."Name: ". $row['tradeName'] . "  Stock: ". $row['inStock'] ."</option>";
                             }
                         } else {
-                            echo "No items are found in the database.";
+                            echo "No items were found in the database.";
                         }
                     ?>
                 </select>
@@ -44,11 +44,11 @@
             <b>Customer Information</b>
 
             <div class="accordion accordion-flush" id="accordionFlushExample">
-                <button class="btn btn-secondary btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                <button class="btn btn-secondary btn-sm" onclick="selectCustomer()" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                     Select Customer
                 </button>
 
-                <button class="btn btn-secondary btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                <button class="btn btn-secondary btn-sm" onclick="addCustomer()" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
                     Add Customer
                 </button>
 
@@ -57,7 +57,7 @@
                     <div class="row g-3">
                         <div class="col-md-4">
                             <label class="form-label">Customer</label>
-                            <select class="form-select form-select-md mb-3" name="customer">
+                            <select id="oldCus" class="form-select form-select-md mb-3" name="customer">
                                 <option value="">Select Customer</option>
                                 <?php
                                     if (mysqli_num_rows($result) > 0) {
@@ -81,27 +81,27 @@
 
                         <div class="col-md-4">
                             <label class="form-label">First Name</label>
-                            <input type="text" class="form-control" name="customerFname">
+                            <input id="input1" type="text" class="form-control" name="customerFname">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Last Name</label>
-                            <input type="text" class="form-control" name="customerLname">
+                            <input id="input2" type="text" class="form-control" name="customerLname">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Email</label>
-                            <input type="email" class="form-control" name="emailAddress">
+                            <input id="input3" type="email" class="form-control" name="emailAddress">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Date of Birth</label>
-                            <input type="date" class="form-control" name="dateOfBirth">
+                            <input id="input4" type="date" class="form-control" name="dateOfBirth">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Gender</label>
-                            <select name="gender" class="form-select">
+                            <select id="input5" name="gender" class="form-select">
                                 <option value="M" selected>Male</option>
                                 <option value="F">Female</option>
                             </select>
@@ -109,7 +109,7 @@
 
                         <div class="col-md-4">
                             <label class="form-label">Contact Number</label>
-                            <input type="text" class="form-control" name="contactNo">
+                            <input id="input6" type="text" class="form-control" name="contactNo">
                         </div>
 
                         <hr>
@@ -117,32 +117,32 @@
 
                         <div class="col-md-4">
                             <label class="form-label">Street</label>
-                            <input type="text" class="form-control" name="street">
+                            <input id="input7" type="text" class="form-control" name="street">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Barangay</label>
-                            <input type="text" class="form-control" name="barangay">
+                            <input id="input8" type="text" class="form-control" name="barangay">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">City</label>
-                            <input type="text" class="form-control" name="city">
+                            <input id="input9" type="text" class="form-control" name="city">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Region</label>
-                            <input type="text" class="form-control" name="region">
+                            <input id="input10" type="text" class="form-control" name="region">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Country</label>
-                            <input type="text" class="form-control" name="country">
+                            <input id="input11" type="text" class="form-control" name="country">
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Zip</label>
-                            <input type="text" class="form-control" name="zip">
+                            <input id="input12" type="text" class="form-control" name="zip">
                         </div>
 
                         <div class="col-12">
@@ -193,5 +193,25 @@
         });
 
     });
+
+    function selectCustomer(){
+        document.getElementById("oldCus").required = true;
+
+        for (let i = 1; i <= 12; i++) {
+            let input = "input" + i;
+            console.log(input);
+            document.getElementById(input).required = false;
+        }
+    }
+
+    function addCustomer(){
+        document.getElementById("oldCus").required = false;
+        
+        for (let i = 1; i <= 12; i++) {
+            let input = "input" + i;
+            console.log(input);
+            document.getElementById(input).required = true;
+        }
+    }
     
 </script>
