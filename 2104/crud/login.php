59a01d86e53b2881926e84a08dc4ea608e2e0636
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-01-06 10:17:27 +0800

    commit

--- 2104/crud/login.php
+++ 2104/crud/login.php
@@ -1,85 +1,34 @@
-<?php 
-    session_start();
-    include('../crud/db_connect.php');
-    switch($_POST['user']){
-        case "login":
-                $email = $_POST['email'];
-                $password = $_POST['password'];
+<?php
+session_start();
+include('../crud/db_connect.php');
 
-        
-                $checkuser = "SELECT * FROM `inventory_users`
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$checkuser = "SELECT * FROM `inventory_users`
                                 WHERE `email`='$email'
                                 LIMIT 1";
 
-                $result = mysqli_query($conn, $checkuser);
+$result = mysqli_query($conn, $checkuser);
 
-                $user = mysqli_fetch_assoc($result);
+$user = mysqli_fetch_assoc($result);
 
-                if (password_verify($password,$user['password'])){
+if (password_verify($password, $user['password'])) {
 
-                        $_SESSION['userID'] = $user['userID'];
-                        $_SESSION['userType'] = $user['userType'];
-                        $_SESSION['userFirstName'] = $user['userFirstName'];
-                        $_SESSION['userLastName'] = $user['userLastName'];
-                        $_SESSION['userName'] = $user['userName'];
-                        $_SESSION['email'] = $user['email'];
-                        $_SESSION['password'] = $user['password'];
+    $_SESSION['userID'] = $user['userID'];
+    $_SESSION['userType'] = $user['userType'];
+    $_SESSION['userFirstName'] = $user['userFirstName'];
+    $_SESSION['userLastName'] = $user['userLastName'];
+    $_SESSION['userName'] = $user['userName'];
+    $_SESSION['email'] = $user['email'];
+    $_SESSION['password'] = $user['password'];
 
-                        echo $_SESSION['email'];
+    echo $_SESSION['email'];
 
-                        header("Location: ../sections/notifications.php");
-                    
-                }else{
-                    header("Location: ../index.php");
-                    $_SESSION['msg'] = "Invalid Email or Password";
-                }
-                    
-               break;
-
-            case "register":
-
-                $userName = $_POST['userName'];
-                $userFirstName = $_POST['userFirstName'];
-                $userLastName = $_POST['userLastName'];
-                $email = $_POST['email'];
-                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                $userType = $_POST['userType'];
-                
-
-
-                $checkuser = "SELECT * FROM `inventory_users` 
-                                WHERE `userName`='$userName'
-                                AND `email`='$email'
-                                LIMIT 1";
-
-                $result = mysqli_query($conn, $checkuser);
-
-                if (mysqli_num_rows($result) == 0) {
-                    
-                    $insertUser = "INSERT INTO `inventory_users`(`userID`, `userType`, `userFirstName`, `userLastName`, `userName`, `email`, `password`)
-                    VALUES ('','$userName','$userType','$userFirstName','$userLastName','$email','$password')";
-            
-                    if (mysqli_query($conn, $insertUser)) {
-                        echo "Created successfully";
-                        header("Location: login.php");
-                    } else {
-                        echo "Error creating user: " . mysqli_error($conn);
-                    } 
-                }else{
-                    header("Location: sections/register.php");
-                }
-    }
-    
+    header("Location: ../sections/notifications.php");
+} else {
+    header("Location: ../index.php");
+    $_SESSION['msg'] = "Invalid Email or Password";
+}
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
