302bd8209a96eee6f31d227e6b988ec140b33d3b
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-01-16 17:02:28 +0800

    security feature

--- 2104/crud/order/update-order.php
+++ 2104/crud/order/update-order.php
@@ -4,124 +4,127 @@
 
     $active = "../../sections/orders.php?orderActive=".$_POST['defaultOrderStatus'];
 
-    $product = $_POST['product'];
-    $quantity = $_POST['quantity'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    /* Update Order quantity if status is changed to cancelled*/
-    if (!isset($_POST['orderStatus'])) {
-        $orderStatus = $_POST['defaultOrderStatus'];
-    }
-    else {
-        $orderStatus = $_POST['orderStatus'];
-        if ($orderStatus == "Cancelled") {
-            foreach($product as $index => $prodID) {
-                $productID = $prodID;
-                $qty = $quantity[$index];
-    
-                /* Query to update InStock */
-                $sql = "UPDATE product
-                            SET inStock = inStock + (?)
-                            WHERE productID = (?) ";
+        $product = $_POST['product'];
+        $quantity = $_POST['quantity'];
 
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param('ii', $qty, $productID);
+        /* Update Order quantity if status is changed to cancelled*/
+        if (!isset($_POST['orderStatus'])) {
+            $orderStatus = $_POST['defaultOrderStatus'];
+        }
+        else {
+            $orderStatus = $_POST['orderStatus'];
+            if ($orderStatus == "Cancelled") {
+                foreach($product as $index => $prodID) {
+                    $productID = $prodID;
+                    $qty = $quantity[$index];
+        
+                    /* Query to update InStock */
+                    $sql = "UPDATE product
+                                SET inStock = inStock + (?)
+                                WHERE productID = (?) ";
 
-                if ($stmt->execute()) {
-                    echo '<br /> Product In Stock is successfully updated';
-                }else {
-                    echo '<br /> Product In Stock is not successfully updated ' . $conn->error;
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param('ii', $qty, $productID);
+
+                    if ($stmt->execute()) {
+                        echo '<br /> Product In Stock is successfully updated';
+                    }else {
+                        echo '<br /> Product In Stock is not successfully updated ' . $conn->error;
+                    }
                 }
             }
         }
-    }
 
-    /* Order Information */
-    $orderID = $_POST['orderID'];
-    $orderDate = $_POST['orderDate'];
-    $shippingDate = date('Y-m-d', strtotime($_POST['shippingDate']));
+        /* Order Information */
+        $orderID = $_POST['orderID'];
+        $orderDate = $_POST['orderDate'];
+        $shippingDate = date('Y-m-d', strtotime($_POST['shippingDate']));
 
-    /* Query to update order information */
-    /* First check if order status is awaiting-approval, then allow admin to approve the order */
+        /* Query to update order information */
+        /* First check if order status is awaiting-approval, then allow admin to approve the order */
 
-    if ($_POST['defaultOrderStatus'] == 'Awaiting-Approval' && $orderStatus != 'Awaiting-Approval') {
-        $approvedBy = $_SESSION['userID'];
+        if ($_POST['defaultOrderStatus'] == 'Awaiting-Approval' && $orderStatus != 'Awaiting-Approval') {
+            $approvedBy = $_SESSION['userID'];
 
-        $sql = "UPDATE orders 
-                    SET orderDate = (?), shipRequiredDate = (?), orderStatus = (?), approvedBy = (?)
-                    WHERE orderID = (?) ";
+            $sql = "UPDATE orders 
+                        SET orderDate = (?), shipRequiredDate = (?), orderStatus = (?), approvedBy = (?)
+                        WHERE orderID = (?) ";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('sssii', $orderDate, $shippingDate, $orderStatus, $approvedBy, $orderID);
+            
+            if ($stmt->execute()) {
+                echo '<br /> Order Information is successfully updated.';
+            } else {
+                echo '<br /> Order Information is not successfully updated. ' . $conn->error;
+            }
+        
+        } else {
+            $sql = "UPDATE orders 
+                        SET orderDate = (?), shipRequiredDate = (?), orderStatus = (?)
+                        WHERE orderID = (?) ";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('sssi', $orderDate, $shippingDate, $orderStatus, $orderID);
+
+            if ($stmt->execute()) {
+                echo '<br /> Order Information is successfully updated.';
+            } else {
+                echo '<br /> Order Information is not successfully updated. ' . $conn->error;
+            }
+        }
+
+        /* Query to update customer information */
+        $sql = "UPDATE customer
+                    SET customerFName = (?), customerLName = (?), dateofBirth = (?), gender = (?), 
+                        contactNo = (?), email = (?)
+                    WHERE customerID = (?)";
 
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param('sssii', $orderDate, $shippingDate, $orderStatus, $approvedBy, $orderID);
+        $stmt->bind_param('ssssssi', $customerFname, $customerLname, $dateOfBirth, $gender, $contactNo, $email, $customerID);
+
+        /* Customer Information */
+        $customerID = $_POST['customerID']; 
+        $customerFname = $_POST['fname'];
+        $customerLname = $_POST['lname'];
+        $email = $_POST['email'];
+        $dateOfBirth = date('Y-m-d', strtotime($_POST['dob']));
+        $gender = $_POST['gender'];
+        $contactNo = $_POST['contactNo'];
+
+        if ($stmt->execute()) {
+            echo '<br /> Customer Address is successfully updated.';
+        } else {
+            echo '<br /> Customer Address is not successfully updated. ' . $conn->error;
+        }
+
+        /* Query to update customer address */
+        $sql = "UPDATE customer_address 
+                    SET street = (?), barangay = (?), city = (?), region = (?), country = (?), zip = (?)
+                    WHERE addressID = (?)";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('ssssssi', $street, $barangay, $city, $region, $country, $zip, $addressID);
+
+        /* Customer Address */
+        $addressID = $_POST['addressID']; 
+        $street = $_POST['street'];
+        $barangay = $_POST['barangay'];
+        $city = $_POST['city'];
+        $region = $_POST['region'];
+        $country = $_POST['country'];
+        $zip = $_POST['zip'];
         
         if ($stmt->execute()) {
-            echo '<br /> Order Information is successfully updated.';
+            echo '<br /> Customer Address is successfully updated.';
         } else {
-            echo '<br /> Order Information is not successfully updated. ' . $conn->error;
+            echo '<br /> Customer Address is not successfully updated. ' . $conn->error;
         }
-    
-    } else {
-        $sql = "UPDATE orders 
-                    SET orderDate = (?), shipRequiredDate = (?), orderStatus = (?)
-                    WHERE orderID = (?) ";
+        $stmt->close();
+    } 
 
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param('sssi', $orderDate, $shippingDate, $orderStatus, $orderID);
-
-        if ($stmt->execute()) {
-            echo '<br /> Order Information is successfully updated.';
-        } else {
-            echo '<br /> Order Information is not successfully updated. ' . $conn->error;
-        }
-    }
-
-    /* Query to update customer information */
-    $sql = "UPDATE customer
-                SET customerFName = (?), customerLName = (?), dateofBirth = (?), gender = (?), 
-                    contactNo = (?), email = (?)
-                WHERE customerID = (?)";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('ssssssi', $customerFname, $customerLname, $dateOfBirth, $gender, $contactNo, $email, $customerID);
-
-    /* Customer Information */
-    $customerID = $_POST['customerID']; 
-    $customerFname = $_POST['fname'];
-    $customerLname = $_POST['lname'];
-    $email = $_POST['email'];
-    $dateOfBirth = date('Y-m-d', strtotime($_POST['dob']));
-    $gender = $_POST['gender'];
-    $contactNo = $_POST['contactNo'];
-
-    if ($stmt->execute()) {
-        echo '<br /> Customer Address is successfully updated.';
-    } else {
-        echo '<br /> Customer Address is not successfully updated. ' . $conn->error;
-    }
-
-    /* Query to update customer address */
-    $sql = "UPDATE customer_address 
-                SET street = (?), barangay = (?), city = (?), region = (?), country = (?), zip = (?)
-                WHERE addressID = (?)";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('ssssssi', $street, $barangay, $city, $region, $country, $zip, $addressID);
-
-    /* Customer Address */
-    $addressID = $_POST['addressID']; 
-    $street = $_POST['street'];
-    $barangay = $_POST['barangay'];
-    $city = $_POST['city'];
-    $region = $_POST['region'];
-    $country = $_POST['country'];
-    $zip = $_POST['zip'];
-    
-    if ($stmt->execute()) {
-        echo '<br /> Customer Address is successfully updated.';
-    } else {
-        echo '<br /> Customer Address is not successfully updated. ' . $conn->error;
-    }
-
-    $stmt->close();
     $conn->close();
 
     header("location: $active");
