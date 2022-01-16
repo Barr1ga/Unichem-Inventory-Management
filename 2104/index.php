f36d2685089a0f7b21e8566d7fc899e35f0abd63
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-01-16 17:34:21 +0800

    add alert in login

--- 2104/index.php
+++ 2104/index.php
@@ -33,7 +33,28 @@ marge123
     <main class="form-signin border shadow">
         <form method="post" action="crud/login.php">
             <img class="mb-4" src="assets/images/Unichem-Logo-Text.svg" alt="" width="200" height="100" />
-            <input type="hidden" name="user" value="login">
+
+            <?php
+            if (isset($_SESSION['msg'])) { ?>
+                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </symbol>
+                </svg>
+
+                <div class="alert alert-danger d-flex align-items-center justify-content-center" role="alert">
+                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+                        <use xlink:href="#exclamation-triangle-fill" />
+                    </svg>
+                    <div>
+                        <?php echo $_SESSION['msg'] ?>
+                    </div>
+                </div>
+            <?php
+                unset($_SESSION['msg']);
+            }
+            ?>
+
             <div class="form-floating">
                 <input type="email" class="form-control" name="email" placeholder="Email Address" required />
                 <label for="floatingInput">Email address</label>
@@ -44,18 +65,13 @@ marge123
                 <label for="floatingPassword">Password</label>
             </div>
 
-            <button type="submit" class="w-100 btn btn-lg btn-primary" type="submit">
+            <button type="submit" class="w-100 btn btn-lg btn-primary mt-1" type="submit">
                 Log in
             </button>
 
-            <?php
-            if (isset($_SESSION['msg'])) {
-                echo "<div class = 'mt-2 text-center text-danger'>" . $_SESSION['msg'] . "</div>";
-                unset($_SESSION['msg']);
-            }
-            ?>
-
-            <p class="mt-5 mb-3 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> | UniChem</p>
+            <p class="mt-5 mb-3 text-muted">&copy; <script>
+                    document.write(new Date().getFullYear())
+                </script> | UniChem</p>
         </form>
     </main>
 </body>
